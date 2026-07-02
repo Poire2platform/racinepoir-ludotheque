1568a4e2ed17ac77a683e68f3309472113a5df9e
Maxime Pearson <maxpearsonplatform@gmail.com>
2026-07-01 20:44:16 -0400

    Create V1 database schema

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,22 @@
 import os
 from flask import Flask
 from dotenv import load_dotenv
-from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
+from .extensions import db, migrate
+
 
 def create_app():
     load_dotenv()
 
     app = Flask(__name__)
 
-    app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+    app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "dev-secret")
     app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
-    
+    migrate.init_app(app, db)
+
     from . import models
 
     from .routes import main
